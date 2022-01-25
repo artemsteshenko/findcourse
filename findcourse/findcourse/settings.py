777b4bfa5c217002cb5f69dba7b50676fd218ac8
Your Name <your@email.com>
2022-01-25 22:58:08 +0300

    Initial

--- findcourse/findcourse/settings.py
+++ findcourse/findcourse/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'findcourse.urls'
+ROOT_URLCONF = 'findcourse.findcourse.urls'
 
 TEMPLATES = [
     {

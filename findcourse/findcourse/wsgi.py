88a5b379a7421f061f3020557c1be681f8209698
Your Name <your@email.com>
2022-01-25 22:02:50 +0300

    Initial

--- findcourse/findcourse/wsgi.py
+++ findcourse/findcourse/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findcourse.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findcourse.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "findcourse.settings"
 
 application = get_wsgi_application()

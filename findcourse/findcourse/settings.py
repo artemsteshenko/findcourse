38f01662c2366d23aad0e1871712072781d8a2fe
Your Name <your@email.com>
2022-01-25 23:02:06 +0300

    Initial

--- findcourse/findcourse/settings.py
+++ findcourse/findcourse/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'findcourse.main',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'findcourse.wsgi.application'
+WSGI_APPLICATION = 'findcourse.findcourse.wsgi.application'
 
 
 # Database

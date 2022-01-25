94e4e74d24c0945450e27dd9aadd6c7873ad4bbb
Your Name <your@email.com>
2022-01-25 20:35:59 +0300

    Initial

--- findcourse/findcourse/settings.py
+++ findcourse/findcourse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+k8)+%+ouwap!q^3bebwxporjs$=u=t+p&hacm2_6w+_ol7*e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--+k8)+%+ouwap!q^3bebwxporjs$=u=t+p&hacm2_6w+_ol7*e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
